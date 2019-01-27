b86090f9bb45964a5c648313f8b753ec7a3bc3f3
saifulsaif <saifulsaif5854@gmail.com>
2019-01-27 16:26:03 +0600

    add-button

--- index.php
+++ index.php
@@ -10,39 +10,46 @@
 	<div id="data">
 		
 	</div>
+    <button id="btn">Show Data</button>
+    <script>
+        var btn = document.getElementById("btn");
+        btn.addEventListener("click", function () {
+            var ajax = new XMLHttpRequest();
+            var method = "GET";
+            var url = "data.php";
+            var asynchronous = true;
+
+            //sending ajax request 
+            ajax.open(method,url,asynchronous);
+            ajax.send();
+
+            //receiving response form data.php
+
+            ajax.onreadystatechange = function()
+            {
+                if(this.readyState == 4 && this.status == 200)
+                {
+                    // mconverting jason back t array;
+                    var data = JSON.parse(this.responseText);
+                    console.log(data);
+                    var html = "";
+                    for(var i=0;i<data.length;i++)
+                    {
+                        var author = data[i].author;
+                        var message = data[i].message;
+
+                        html +="<p>"+author+"  = ";
+                        html +=""+message+"</p> ";
+                    }
+                    document.getElementById('data').innerHTML = html;
+                }
+            }
+        });
+    </script>
 	<script>
+
 	//ajax call
-    var ajax = new XMLHttpRequest();
-    var method = "GET";
-    var url = "data.php";
-    var asynchronous = true;
-
-    //sending ajax request 
-    ajax.open(method,url,asynchronous);
-    ajax.send();
-
-    //receiving response form data.php
-
-    ajax.onreadystatechange = function()
-    {
-    	if(this.readyState == 4 && this.status == 200)
-    	{
-    		// mconverting jason back t array;
-    		var data = JSON.parse(this.responseText);
-    		console.log(data);
-
-    		var html = "";
-    		for(var i=0;i<data.length;i++)
-    		{
-    			var author = data[i].author;
-    			var message = data[i].message;
-
-    			html +="<p>"+author+"  = ";
-    			html +=""+message+"</p> ";
-    		}
-    		document.getElementById('data').innerHTML = html;
-    	}
-    }
+    
 
 </script>
 </body>
